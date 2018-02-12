8fd41442236ac679f6e6e5b80420fb1cd354c650
Unknown <mtbthebest11@gmail.com>
2018-02-12 14:56:08 +0900

    hello

--- basics/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char const *argv[])
-{
-    std::cout << "Hello" << '\n';
-    return 0;
-}
-
---
+++ basics/src/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hello World" << '\n';
+    for(int i =0 ; i<=5; i++){
+        std::cout << "Value of i: "<<i << endl;
+    }
+    
+    
+    return 0;
+}
+
