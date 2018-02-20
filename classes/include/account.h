be0cea26d0a3ec803bc557ab8caf353aa910354e
Unknown <mtbthebest11@gmail.com>
2018-02-21 03:29:43 +0900

    classes

---
+++ classes/include/account.h
@@ -0,0 +1,16 @@
+#ifndef _ACCOUNT_
+#define _ACCOUNT_
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Account{
+    private:
+        string name; unsigned long nr; double balance;
+    public:
+        bool init(const string&, unsigned long, double);
+        void display();
+};
+
+#endif 
+
