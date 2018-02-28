32efbd3bc822ff180e41c1e0160de65a9c760189
Unknown <mtbthebest11@gmail.com>
2018-02-28 19:01:47 +0900

    classes

---
+++ classes/src/main.cpp
@@ -0,0 +1,10 @@
+#include "/home/mtb/Projects/C++/classes/include/Account.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    Account current1, current2;
+    current1.init("Cheers, Mary", 1234567, -1200.99);
+    return 0;
+}
